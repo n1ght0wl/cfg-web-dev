b591fee33abfef575255cc3e4a63cdc290938d7e
neternefer <nodzynska@gmail.com>
2023-02-21 17:21:24 +0100

    Enable mardi mode on load

---
+++ js/switch.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+    const body = document.querySelector("body");
+    body.classList.add("color-mode");
+}
+
+/*TODO
+- add switch icon
+- enable switching on all sites
+- clean up css
+- use media wueries for themes
+*/ 
